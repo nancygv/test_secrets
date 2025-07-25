82f812700f21fa171b9cc72dda3844b057d97181
nancygv <110550654+nancygv@users.noreply.github.com>
2025-07-25 16:27:55 -0600

    fix: update secrets.py to use lowercase username for validation and adjust GitHub Actions workflow to use secrets

--- secrets.py
+++ secrets.py
@@ -4,7 +4,7 @@ parser.add_argument('usuario')
 parser.add_argument('password')
 args = parser.parse_args()
 
-if args.usuario == "Nancy" and args.password == "morado":
+if args.usuario == "nancy" and args.password == "morado":
     print("Hola acceso correcto")
 else:
     print("Hola acceso incorrecto")
